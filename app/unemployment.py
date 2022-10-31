1c22b4cdae5300dcbc87ee04c3a3acf5e47925bf
emm331 <emm331@georgetown.edu>
2022-10-31 17:19:43 -0400

    Update unemployment.py

--- app/unemployment.py
+++ app/unemployment.py
@@ -1,40 +1,32 @@
 
-# this is the app/unemployment_report.py file
-
 import os
 import json
 from pprint import pprint
 
+from dotenv import load_dotenv
 import requests
+from plotly.express import line
 
-# this is where we specify the name of API variable
+load_dotenv()
+
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
-request_url = "https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
-
-from getpass import getpass
-
-API_KEY = getpass("Please input your AlphaVantage API Key: ") 
+request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
 
 response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 print(type(parsed_response))
-pprint(parsed_response)
+#pprint(parsed_response)
+
+#breakpoint()
 
 data = parsed_response["data"]
-
-# add breakpoint as a debugging technique to enter into interactive debugging console
-
- # breakpoint()
-
-#latest = parsed_response["data"][0]
-#print(latest)
 
 
 # Challenge A
 #
-# What is the most recent unemployment rate? And the corresponding date? 
+# What is the most recent unemployment rate? And the corresponding date?
 # Display the unemployment rate using a percent sign.
 
 print("-------------------------")
@@ -43,27 +35,9 @@ print("LATEST UNEMPLOYMENT RATE:")
 print(f"{data[0]['value']}%", "as of", data[0]["date"])
 
 
-# Challenge B
-# 
-# What is the average unemployment rate for all months during this calendar year?
-# ... How many months does this cover?
-
-from statistics import mean
-
-this_year = [d for d in data if "2022-" in d["date"]]
-
-rates_this_year = [float(d["value"]) for d in this_year]
-#print(rates_this_year)
-
-print("-------------------------")
-print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
-print("NO MONTHS:", len(this_year))
-
 # Challenge C
-# 
+#
 # Plot a line chart of unemployment rates over time.
-
-from plotly.express import line
 
 dates = [d["date"] for d in data]
 rates = [float(d["value"]) for d in data]
